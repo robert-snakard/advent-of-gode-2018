381bbd8b1552b9f94bcb012c76cb0a9ba1a46a6b
EC2 Default User <robert-snakard@github.com>
2019-01-25 22:13:57 +0000

    Parsing input done

--- 4-repose-record/main.go
+++ 4-repose-record/main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
-//	"errors"
+	"errors"
 	"fmt"
 	"io/ioutil"
+	"sort"
 	"strings"
-//	"strconv"
+	"strconv"
+	"time"
 
 	"../shared"
 )
 
-type interval struct {
+type Event struct {
+	time time.Time
+	state State
+	guard_id int
+}
+
+type Interval struct {
 	start int
 	end int
 }
@@ -23,25 +31,76 @@ func main() {
 	if data[len(data)-1] == "" {
 		data = data[:len(data)-1]
 	}
-	sleep_intervals, err := ParseData(data)
+	events, err := ParseIntoEvents(data)
 	check.Check(err)
 
-	fmt.Println("Part 1 Solution:", part_1(sleep_intervals))
+	fmt.Println("Part 1 Solution:", part_1(events))
 }
 
-func part_1(sleep_intervals map[int][]interval) int{
+func part_1(events []Event) int{
 	return 4
 }
 
-func ParseData(data []string) (map[int][]interval, error){
-	sleep_intervals := make(map[int][]interval)
-	sleep_intervals[1] = []interval{}
+type State int
+const (
+	NewGuard    State = 0
+	FallsAsleep State = 1
+	WakesUp     State = 2
+)
 
-	// Use old style for loop so we can increment i inside the loop
-	for i := 0; i < 5; {
-		fmt.Println("looping:", i)
-		i++
+func ParseIntoEvents(data []string) ([]Event, error){
+	var events []Event
+	for _, line := range data {
+		var err error
+		var cur_event Event
+
+		// Line halves if of the form
+		//     [0] = date and time
+		//     [1] = string that describes state
+		line_halves := strings.Split(line, "]")
+
+		time_layout := "[2006-01-02 15:04"
+		cur_event.time, err = time.Parse(time_layout, line_halves[0])
+		check.Check(err)
+
+		if strings.Contains(line_halves[1], "begins shift") {
+			cur_event.state = NewGuard
+		} else if strings.Contains(line_halves[1], "falls asleep") {
+			cur_event.state = FallsAsleep
+		} else if strings.Contains(line_halves[1], "wakes up") {
+			cur_event.state = WakesUp
+		} else {
+			return []Event{}, errors.New(fmt.Sprintf("Error parsing input: Unable to",
+			                                         "determine state of %s", line))
+		}
+
+		// Split to get id is of the form
+		//     [0] - ""
+		//     [1] - "Guard"
+		//     [2] - "#1234"
+		//     [3] - "begins"
+		//     [4] - "shift"
+		if cur_event.state == NewGuard {
+			split_to_get_id := strings.Split(line_halves[1], " ")
+			cur_event.guard_id, err = strconv.Atoi(split_to_get_id[2][1:])
+			check.Check(err)
+		}
+
+		events = append(events, cur_event)
 	}
 
-	return sleep_intervals, nil
+	sort.Sort(byDateTime(events))
+	return events nil
+}
+
+// Helper Functions for sorting
+type byDateTime []ParsedLine
+func (s byDateTime) Len() int {
+	return len(s)
+}
+func (s byDateTime) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+func (s byDateTime) Less(i, j int) bool {
+	return s[i].time.Before(s[j].time)
 }
